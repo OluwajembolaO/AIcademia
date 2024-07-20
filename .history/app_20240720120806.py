392aa0562a9d512c9c6b50d2db129bd9d7499f0e
Jem <116439666+OluwajembolaO@users.noreply.github.com>
2024-07-20 12:14:41 -0400

    Setting Flask And Venv

---
+++ .history/app_20240720120806.py
@@ -0,0 +1,2 @@
+from flask import Flask
+from ffrom flask_sqlalchemy import SQLAlchemy
---
+++ .history/app_20240720120916.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+
+
+
+
+if __name__ == '__main__':
+    
