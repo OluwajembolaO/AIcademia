3a94ab3b1fbb335cb5fe48d9765b17aef62a0f84
Jem <116439666+OluwajembolaO@users.noreply.github.com>
2024-07-22 19:39:18 -0400

    Pushing

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = secret_key
 
 # Flask-Mail configuration
-app.config['MAIL_SERVER'] = 'smtp.yourmailserver.com'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'your_email@example.com'
